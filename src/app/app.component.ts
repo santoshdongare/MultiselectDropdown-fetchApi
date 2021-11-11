8d671d4ec39ac1fb6600894a38841a30f141b28e
Santosh <santosh.dongare@genpact.com>
2021-11-11 20:45:38 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import {UserdataService} from './userdata.service';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'apifetchdata1';
+
+  dropdownList:any = [];
+  dropdownSettings:IDropdownSettings={};
+  userData:any=[];
+
+  constructor(private userdataService:UserdataService){
+
+    this.userdataService.getUserData().subscribe(data=>{
+      console.log(data);
+      this.userData=data;
+    })
+    
+  }
+  ngOnInit(){
+
+    this.dropdownList = [
+      { item_id: 1, item_text: 'Mumbai' },
+      { item_id: 2, item_text: 'Kolhapur' },
+      { item_id: 3, item_text: 'Pune' },
+      { item_id: 4, item_text: 'Nagpur' },
+      { item_id: 5, item_text: 'Nashik' }
+    ];
+    
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 5,
+      allowSearchFilter: true
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgMultiSelectDropDownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
